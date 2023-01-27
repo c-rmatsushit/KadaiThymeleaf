dc697d5756039d04cbd6098c0be41397d5640d07
Ryunosuke Matsushita <c-rmatsushit@192.168.10.104>
2023-01-27 20:05:46 +0900

    修正

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,4 +1,5 @@
 package com.techacademy;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +8,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
-	 @GetMapping("/input")
-	    public String getForm(Model model) {
-	        // 文字列をModelに登録
-	        model.addAttribute("msg", "値を入力してください。");
-	        // formに画面遷移
-	        return "input";
-	    }
-
-	    @PostMapping("/input")
-	    public String postForm(@RequestParam("val") String val, Model model) {
-	        model.addAttribute("msg", "入力された値は「" + val + "」です。");
-	        model.addAttribute("val", val);
-	        return "input";
-	    }
+	@GetMapping("/input")
+	public String getInput(Model model) {
+		// 文字列をModelに登録
+		model.addAttribute("msg", "値を入力してください。");
+		// formに画面遷移
+		return "input";
 	}
+
+	@PostMapping("/input")
+	public String postForm(@RequestParam("val") String val, Model model) {
+		model.addAttribute("msg", "入力された値は「" + val + "」です。");
+		model.addAttribute("val", val);
+		return "input";
+	}
+}
