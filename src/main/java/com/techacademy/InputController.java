7d3388d8d41b202a7ac73e5ae0fc50c10e50f625
Ryunosuke Matsushita <c-rmatsushit@192.168.10.104>
2023-01-27 19:27:01 +0900

    フォーム作成

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,5 +1,24 @@
 package com.techacademy;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class InputController {
+	 @GetMapping("/input")
+	    public String getForm(Model model) {
+	        // 文字列をModelに登録
+	        model.addAttribute("msg", "値を入力してください。");
+	        // formに画面遷移
+	        return "input";
+	    }
 
-}
+	    @PostMapping("/input")
+	    public String postForm(@RequestParam("val") String val, Model model) {
+	        model.addAttribute("msg", "入力された値は「" + val + "」です。");
+	        model.addAttribute("val", val);
+	        return "input";
+	    }
+	}
