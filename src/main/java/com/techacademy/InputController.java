2ad29568354036cc1f6522340e8d88e5a92b8bb8
Ryunosuke Matsushita <c-rmatsushit@192.168.10.104>
2023-01-30 21:25:17 +0900

    変更

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,21 +1,22 @@
 package com.techacademy;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
 
-	@GetMapping("/input(previous=${val})")
-	public String getInput(@PathVariable String val ,Model model) {
+	@GetMapping("/input")
+	public String getInput(@RequestParam("previous") String previous, Model model) {
 		// 文字列をModelに登録
 		model.addAttribute("msg2", "値を入力してください。");
-		model.addAttribute("msg3", "入力された値は「" + val + "」です。");
+		model.addAttribute("msg3", "前回入力された値は" + previous + " でした。");
 		// formに画面遷移
 		return "input";
 	}
@@ -24,6 +25,6 @@ public class InputController {
 	public String getInput(@RequestParam("val") String val, Model model, RedirectAttributes redirectAttributes) {
 		model.addAttribute("val", val);
 		redirectAttributes.addFlashAttribute("val", val);
-		return "redirect:output";
+		return "redirectz:output";
 	}
 }
